7ce5de793544004cb1d4a583d1bf4e258f15421f
Markov Alexey <markovalex95@gmail.com>
2016-08-12 14:33:46 +0300

    Сделал кнопку с индикатором прогресса для асинхронных операций

---
+++ TMBot/Views/AsyncButton.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TMBot.Utilities.MVVM;
+
+namespace TMBot.Views
+{
+	/// <summary>
+	/// Логика взаимодействия для AsyncButton.xaml
+	/// </summary>
+	public partial class AsyncButton : Button
+	{
+	 
+        public AsyncButton()
+		{
+
+            InitializeComponent();
+		}
+	}
+}

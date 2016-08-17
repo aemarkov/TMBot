54a3ce85091c175b4842dbae2dddf97b9a73a3c7
Markov Alexey <markovalex95@gmail.com>
2016-08-17 15:39:54 +0300

    Вынес всю логику из SellWorker в BaseWorker, в SellWorker оставил только то, что относится непосредственно к продажам

--- TMBot/Workers/SellWorker.cs
+++ TMBot/Workers/SellWorker.cs
@@ -28,255 +28,62 @@ namespace TMBot.Workers
 	/// предметов в фоне
 	/// </summary>
 	/// <typeparam name="TTMAPI">Класс АПИ площадки</typeparam>
-	public class SellWorker<TTMAPI,TSteamAPI> : PropertyChangedBase where TTMAPI: ITMAPI where TSteamAPI: ISteamAPI
-	{
-		//Апи для выполнения запросов
-		private readonly ITMAPI tmApi;
-	    private readonly ISteamAPI steamApi;
-
-        //Список продаваемых предметов
-        #region Trades
-        private object _tradesLock = new object();
-	    private ObservableCollection<TradeItemViewModel> _trades;
-
-	    public ObservableCollection<TradeItemViewModel> Trades
+	public class SellWorker<TTMAPI,TSteamAPI> : BaseWorker<TTMAPI, TSteamAPI, Trade> where TTMAPI : ITMAPI where TSteamAPI : ISteamAPI
+    {
+        //Показывает сообщение об ошибке
+	    protected override void ShowErrorMessage(string error_reason)
 	    {
-	        get { return _trades; }
-	        private set
-	        {
-	            _trades = value;
-	            BindingOperations.EnableCollectionSynchronization(_trades, _tradesLock);
-	        }
+	        MessageBox.Show($"Не удалось начать продажу: {error_reason}", "Не удалось начать продажу", MessageBoxButton.OK,
+	            MessageBoxImage.Warning);
 	    }
 
-        #endregion
-
-        /// <summary>
-        /// Запущен ли поток обновления
-        /// </summary>
-        #region IsRunning
-        public bool IsRunning
+        //Получает список трейдов
+	    protected override ICollection<Trade> GetTMItems()
 	    {
-	        get { return _isRunning; }
-            private set { _isRunning = value; NotifyPropertyChanged(); }
-	    }
-        private volatile bool _isRunning;
-        #endregion
-
-
-        /// <summary>
-        /// Текущий выделенный элемент списка
-        /// </summary>
-        #region LastUpdateItemIndex
-        public TradeItemViewModel LastUpdateItem
-	    {
-            get { return _lastUpdatedItem; }
-            set { _lastUpdatedItem = value; NotifyPropertyChanged(); }
-	    }
-	    private TradeItemViewModel _lastUpdatedItem;
-        #endregion
-
-
-	    public float OffsetPercentage
-	    {
-
-            get { return _offsetPercentage;}
-            set { _offsetPercentage = value; NotifyPropertyChanged(); }
-	    }
-
-	    private float _offsetPercentage;
-
-        //Фоновый поток обновления цен
-        private Thread _workThread;
-
-
-		public SellWorker()
-		{
-            //_repository = new ItemsRepository();
-			tmApi = TMFactory.GetInstance<TMFactory>().GetAPI<TTMAPI>();
-		    steamApi = SteamFactory.GetInstance<SteamFactory>().GetAPI<TSteamAPI>();
-            Trades = new ObservableCollection<TradeItemViewModel>();
+	        return tmApi.GetTrades();
         }
 
-        /// <summary>
-        /// Запуск обновления цен
-        /// </summary>
-        public void Begin()
+        //Находит в базе предмет, соответствующий трейду
+	    protected override Item GetDbItem(ItemsRepository repository, Trade api_item)
+	    {
+            return repository.GetById(api_item.i_classid, api_item.ui_real_instance);
+        }
+
+        //Получает цену предмета
+	    protected override int GetItemMyPrice(Trade api_item)
+	    {
+	        return (int)api_item.ui_price*100;
+	    }
+
+        protected override bool GetItemNewPrice(TradeItemViewModel item, int tm_price, ref int myNewPrice)
         {
-            /* Получаем списко трейдов и список 
-             * всех предметов из БД.
-             * Выбираем те предметы, которые сейчас
-             * продаются, чтобы получить из них 
-             * настройки мин, макс цены
-             */
-
-            try
-            {
-                var repository = new ItemsRepository();
-                Trades.Clear();
-
-                var trades = tmApi.GetTrades();
-
-                foreach (var trade_item in trades)
-                {
-                    Item db_item = repository.GetById(trade_item.i_classid, trade_item.ui_real_instance);
-
-                    //Заполняем поля
-                    var item = Mapper.Map<Trade, TradeItemViewModel>(trade_item);
-
-                    //TODO: сделать нормально
-                    //Цена почему-то не в копейках, а в рублях double
-                    item.MyPrice = (int) (trade_item.ui_price*100);
-
-                    if (db_item != null)
-                    {
-                        Mapper.Map<Item, TradeItemViewModel>(db_item, item);
-                    }
-                    else
-                    {
-                        //Такого предмета нет в БД, создадим новый
-                        db_item = new Item()
-                        {
-                            ClassId = item.ClassId,
-                            InstanceId = item.IntanceId
-                        };
-
-                        repository.Create(db_item);
-                    }
-
-                    item.ImageUrl = steamApi.GetImageUrl(item.ClassId);
-                    item.PropertyChanged += Item_PropertyChanged;
-                    Trades.Add(item);
-                }
-
-
-                IsRunning = true;
-                _workThread = new Thread(worker_thread);
-                _workThread.Start();
-
-            }
-            catch (BadKeyException)
-            {
-                MessageBox.Show("Не удалось начать продажу: неверный API-key", "Не удалось начать продажу",
-                      MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Не удалось начать продажу: неизвестная ошибка", "Не удалось начать продажу",
-                      MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-        }
-
-        /// <summary>
-        /// Остановка обновления цен
-        /// </summary>
-        public void End()
-        {
-            IsRunning = false;
-        }
-
-        //Сохранение значений из таблицы в БД
-        private void Item_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            var item = (TradeItemViewModel) sender;
-            if (e.PropertyName == "CountLimint" || e.PropertyName == "PriceLimit")
-            {
-                var repository = new ItemsRepository();
-                var dto_item = repository.GetById(item.ClassId, item.IntanceId);
-                dto_item.CountLimit = item.CountLimint;
-                dto_item.PriceLimit = item.PriceLimit;
-                repository.Update(dto_item);
-            }
-        }
-
-		//Функция потока
-		private async void worker_thread()
-		{
-			/* Необходимо выполнять не более 3х запросов в секунду.
-			 * Запрос - ItemRequest
-			 * Запрос - изменение цены
-			 * Поэтому ограничим 1 предмет в секунду
-			 * 
-			 * Т.к. операции выполняются какое-то время, то ожидание в 1с
-			 * после выполнения операции приведет к тому, что цикл обработки
-			 * будет медленнее, чем 1с.
-			 * 
-			 * Поэтому замерем время, а потом подаждем оставшееся время
-			 */
-
-			while (IsRunning)
-			{
-				foreach (var item in Trades)
-				{
-					await FixedTimeCall.Call(()=>{
-                        update_price(item);
-					});
-
-				    LastUpdateItem = item;
-
-                    if (!IsRunning)
-                        return;
-
-                }
-			}
-		}
-    
-        //Обновляет цену предмета
-	    private void update_price(TradeItemViewModel item)
-	    {
-	        try
-	        {
-                //Находим минимальную цену этого предмета на площадке
-                int? _minPrice = PriceCounter.GetMinSellPrice<TTMAPI>(item.ClassId, item.IntanceId);
-	            int minPrice;
-
-	            if (_minPrice == null)
-	            {
-	                //Товар не найден на площадке, значит единственный  - наш товар
-                    //Значит мы ничего не делаем
-	                return;
-	            }
-	            else
-	                minPrice = (int) _minPrice;
-
-                //Уменьшаем ее на одну копейку
-                item.TMPrice = minPrice;
-                int new_my_price;
-
-                /* Если минимальная цена меньше текущей - делаем нашу меньше минимальной на 
+            /* Если минимальная цена меньше текущей - делаем нашу меньше минимальной на 
                  * 1 коп.
                  * 
                  * Если минимальная - наша, то увеличиваем цену (до минимальной - 1коп) только
                  * если разница больше 10%
                  */
-	            if (minPrice < item.MyPrice || ((minPrice - item.MyPrice)/(float) item.MyPrice > OffsetPercentage))
-	            {
-	                new_my_price = minPrice - 1;
-	            }
-	            else
-	            {
-	                //Цену менять не надо
-                    return;
-	            }
-
-	            if (new_my_price < item.PriceLimit)
-                    new_my_price = item.PriceLimit;
-
-                //Обновляем цену предмета
-                //TODO: убедиться, что itemid - это точно ui_id
-                tmApi.SetPrice(item.ItemId, (int)new_my_price);
-
-                //Обновляем модель
-	            item.MyPrice = new_my_price;
-	            item.TMPrice = minPrice;
-
-            }
-            catch (Exception)
+            if (tm_price < item.MyPrice || ((tm_price - item.MyPrice) / (float)item.MyPrice > OffsetPercentage))
             {
-
-                Log.e("Произошла ошибка при обновлении цены предмета {0}_{1}",item.ClassId,item.IntanceId);
+                myNewPrice = tm_price - 1;
+            }
+            else
+            {
+                //Цену менять не надо
+                myNewPrice = item.MyPrice;
+                return false;
             }
 
+            if (myNewPrice < item.PriceLimit)
+                myNewPrice = item.PriceLimit;
+
+            return true;
         }
+
+        protected override int? GetItemTMPrice(TradeItemViewModel item)
+        {
+            return PriceCounter.GetMinSellPrice<TTMAPI>(item.ClassId, item.IntanceId);
+        }
+
     }
 }
